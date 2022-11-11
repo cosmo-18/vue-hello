19833ba91d712e8db2611890b421885c12219884
cosmo-18 <cosimo.simonesolfrizzi@gmail.com>
2022-11-11 01:29:07 +0100

    all code + bonus

---
+++ assets/script/main.js
@@ -0,0 +1,9 @@
+var app = new Vue (
+    {
+        el: '#root',
+        data: {
+            message:'HELLO BOOLEANERS',
+            img:'https://i.picsum.photos/id/621/200/200.jpg?hmac=nKMlm18QmBAMa_wHA70nsMBpYKk0pU0C832236i2oMM'
+        }
+    }
+)
